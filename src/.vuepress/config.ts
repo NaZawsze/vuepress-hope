e1d34c24caca8bef2bd7d0320c20ed63f5de2f1e
NaZawsze <yiyang88663391@163.com>
2023-12-26 10:54:10 +0000

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,8 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { autoCatalogPlugin } from "vuepress-plugin-auto-catalog";
+import { hopeTheme } from "vuepress-theme-hope";
+
 
 export default defineUserConfig({
   base: "/",
@@ -9,7 +11,7 @@ export default defineUserConfig({
   title: "败犬文档库",
   description: "记录技术理论以及项目方案的文档库.",
 
-  theme: ({
+  theme: hopeTheme({
     iconAssets: [
       "https://at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
       "iconfont",
